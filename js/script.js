31f030e6caaf83939719c51355567f83cdcaf06e
Willie <write2willie@gmail.com>
2021-03-26 00:52:27 -0400

    commit #4: fixed temperature that showed in negative range, march262021, 1251am

--- js/script.js
+++ js/script.js
@@ -9,11 +9,12 @@ let currentHumidty= $("#humidity");
 let currentWSpeed=$("#windSpeed");
 
 //click events
-$("#searchButton").on("click",currentWeather);
+ $("#searchButton").on("click",currentWeather);
 
 let apiKey = "f0c6b76539b7811c6d773e5325562769";
 
 function currentWeather(){
+
     let city = searchCity.val().trim();
     //get a data from api
     let queryURL= `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
@@ -22,29 +23,30 @@ function currentWeather(){
     .then( response => {
         return response.json();
     }).then(function(response){
-        let data = response;
+        // let data = response;
 
-       //get city, date, and weather icon from api parse.
+       
 
+        let weatherIcon= response.weather[0].icon;
+        let iconurl="https://openweathermap.org/img/wn/"+weatherIcon +"@2x.png";
+
+
+        //get city, date, and weather icon from api parse.
         //date
         let date = moment().format("MM/DD/YYYY"); 
 
          //icon
-         let weatherIcon= data.weather[0].icon;
-         let iconurl="https://openweathermap.org/img/wn/"+weatherIcon +"@2x.png";
-
+        
         //current city, date and icon
-        $(currentCity).html(data.name +" ("+date+")" + "<img src="+iconurl+">");
+        $(currentCity).html(response.name +" ("+date+")" + "<img src="+iconurl+">");
        
-        let fahrenheitTemp = (data.main.temp - 273.15) * 1.80 + 32;
+        let fahrenheitTemp = response.main.temp;
+        console.log("f" + fahrenheitTemp)
         $(currentTemp).html((fahrenheitTemp).toFixed(2)+"&#8457");
-        $(currentHumidty).html(data.main.humidity+"%");
+        $(currentHumidty).html(response.main.humidity+"%");
 
-        let currentWindSpeed=(data.wind.speed*2.237).toFixed(1);
+        let currentWindSpeed=(response.wind.speed*2.237).toFixed(1);
         $(currentWSpeed).html(currentWindSpeed+" MPH");
-
-       
-  
     });
 }
 
